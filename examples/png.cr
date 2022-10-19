4ea269a58386d0a7cded9ed7a6316d5fe9ac674d
soya_daizu <soya_daizu@protonmail.com>
2022-10-19 21:50:07 +0900

    Fix typo in example

--- examples/png.cr
+++ examples/png.cr
@@ -4,4 +4,4 @@ require "../src/goban/exporters/png"
 qr = Goban::QRCode.encode_string("Hello World!")
 puts "Exporting with targeted size: 500"
 size = Goban::PNGExporter.export(qr, "test.png", 500)
-puts "Actual QR code size: #{size}"
+puts "Actual QR Code size: #{size}"
