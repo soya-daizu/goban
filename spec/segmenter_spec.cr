5187c65f8aa2ae6b88746434a9bca48a89b3f375
soya_daizu <soya_daizu@proton.me>
2023-02-08 08:57:10 +0900

    Update spec

--- spec/segmenter_spec.cr
+++ spec/segmenter_spec.cr
@@ -2,69 +2,69 @@ require "./spec_helper"
 
 module Goban
   describe Segment::Segmenter do
-    describe ".segment_text_optimized" do
+    describe ".segment_text_optimized_qr" do
       it "optimizes all-numeric string" do
-        segments, version = Segment::Segmenter.segment_text_optimized(ALL_NUMERIC_STR, ECC::Level::Medium)
+        segments, version = Segment::Segmenter.segment_text_optimized_qr(ALL_NUMERIC_STR, ECC::Level::Medium)
         segments.should eq(ALL_NUMERIC_SEGS)
         version.should eq(1)
       end
 
       it "optimizes all-alphanumeric string" do
-        segments, version = Segment::Segmenter.segment_text_optimized(ALL_ALPHANUMERIC_STR, ECC::Level::Medium)
+        segments, version = Segment::Segmenter.segment_text_optimized_qr(ALL_ALPHANUMERIC_STR, ECC::Level::Medium)
         segments.should eq(ALL_ALPHANUMERIC_SEGS)
         version.should eq(1)
       end
 
       it "optimizes all-byte string" do
-        segments, version = Segment::Segmenter.segment_text_optimized(ALL_BYTE_STR, ECC::Level::Medium)
+        segments, version = Segment::Segmenter.segment_text_optimized_qr(ALL_BYTE_STR, ECC::Level::Medium)
         segments.should eq(ALL_BYTE_SEGS)
         version.should eq(1)
       end
 
       it "optimizes all-kanji string" do
-        segments, version = Segment::Segmenter.segment_text_optimized(ALL_KANJI_STR, ECC::Level::Medium)
+        segments, version = Segment::Segmenter.segment_text_optimized_qr(ALL_KANJI_STR, ECC::Level::Medium)
         segments.should eq(ALL_KANJI_SEGS)
         version.should eq(1)
       end
 
       it "optimizes [numeric/alphanumeric] string to [alphanumeric]" do
-        segments, version = Segment::Segmenter.segment_text_optimized(NUMERIC_ALPHANUMERIC_1_STR, ECC::Level::Medium)
+        segments, version = Segment::Segmenter.segment_text_optimized_qr(NUMERIC_ALPHANUMERIC_1_STR, ECC::Level::Medium)
         segments.should eq(NUMERIC_ALPHANUMERIC_1_SEGS)
         version.should eq(1)
       end
 
       it "optimizes [numeric/alphanumeric] string to [numeric/alphanumeric]" do
-        segments, version = Segment::Segmenter.segment_text_optimized(NUMERIC_ALPHANUMERIC_2_STR, ECC::Level::Medium)
+        segments, version = Segment::Segmenter.segment_text_optimized_qr(NUMERIC_ALPHANUMERIC_2_STR, ECC::Level::Medium)
         segments.should eq(NUMERIC_ALPHANUMERIC_2_SEGS)
         version.should eq(1)
       end
 
       it "optimizes [numeric/byte] string to [byte]" do
-        segments, version = Segment::Segmenter.segment_text_optimized(NUMERIC_BYTE_1_STR, ECC::Level::Medium)
+        segments, version = Segment::Segmenter.segment_text_optimized_qr(NUMERIC_BYTE_1_STR, ECC::Level::Medium)
         segments.should eq(NUMERIC_BYTE_1_SEGS)
         version.should eq(1)
       end
 
       it "optimizes [numeric/byte] string to [numeric/byte]" do
-        segments, version = Segment::Segmenter.segment_text_optimized(NUMERIC_BYTE_2_STR, ECC::Level::Medium)
+        segments, version = Segment::Segmenter.segment_text_optimized_qr(NUMERIC_BYTE_2_STR, ECC::Level::Medium)
         segments.should eq(NUMERIC_BYTE_2_SEGS)
         version.should eq(1)
       end
 
       it "optimizes [alphanumeric/byte] string to [byte]" do
-        segments, version = Segment::Segmenter.segment_text_optimized(ALPHANUMERIC_BYTE_1_STR, ECC::Level::Medium)
+        segments, version = Segment::Segmenter.segment_text_optimized_qr(ALPHANUMERIC_BYTE_1_STR, ECC::Level::Medium)
         segments.should eq(ALPHANUMERIC_BYTE_1_SEGS)
         version.should eq(1)
       end
 
       it "optimizes [alphanumeric/byte] string to [alphanumeric/byte]" do
-        segments, version = Segment::Segmenter.segment_text_optimized(ALPHANUMERIC_BYTE_2_STR, ECC::Level::Medium)
+        segments, version = Segment::Segmenter.segment_text_optimized_qr(ALPHANUMERIC_BYTE_2_STR, ECC::Level::Medium)
         segments.should eq(ALPHANUMERIC_BYTE_2_SEGS)
         version.should eq(1)
       end
 
       it "optimizes [kanji/byte/alphanumeric] string" do
-        segments, version = Segment::Segmenter.segment_text_optimized(SAMPLE_STR, ECC::Level::Medium)
+        segments, version = Segment::Segmenter.segment_text_optimized_qr(SAMPLE_STR, ECC::Level::Medium)
         segments.should eq(SAMPLE_SEGS)
         version.should eq(2)
       end
