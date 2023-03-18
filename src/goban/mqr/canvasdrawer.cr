351f7117ca38e40a9a9099717d27ff43e835b193
soya_daizu <soya_daizu@proton.me>
2023-03-19 06:51:00 +0900

    refactor: restructure CanvasDrawer

--- src/goban/mqr/canvasdrawer.cr
+++ src/goban/mqr/canvasdrawer.cr
@@ -1,50 +1,42 @@
-require "../abstract/canvasdrawer"
-
 struct Goban::MQR < Goban::AbstractQR
   # Handles painting each Micro QR Code modules on a canvas.
-  struct CanvasDrawer < AbstractQR::CanvasDrawer
-    @mask : Mask
-    @size : Int32
-
-    # Creates a blank canvas with the given version and error correction level.
-    def initialize(@version : Version, @ecl : ECC::Level)
-      @size = @version.symbol_size
-      @canvas = Matrix(UInt8).new(@size, @size, 0)
-      @mask = Mask.new(0) # Temporal value
-    end
+  module CanvasDrawer
+    extend AbstractQR::CanvasDrawer
 
     # Draws all function patterns on the canvas.
     # The function patterns are:
     #
     # - Finder patterns on the top left corner
     # - Timing patterns in both directions
-    protected def draw_function_patterns
-      @canvas[0, 0, 9, 9] = 0xc0
+    protected def self.draw_function_patterns(canvas : Matrix(UInt8))
+      canvas[0, 0, 9, 9] = 0xc0
 
-      draw_pattern(0, 0, FINDER_PATTERN, 7)
+      self.draw_pattern(canvas, 0, 0, FINDER_PATTERN, 7)
 
-      draw_timing_pattern(0, @size - 8)
+      self.draw_timing_pattern(canvas, 0, canvas.size - 8)
     end
 
     # Draws data bits from the given data codewords.
-    protected def draw_data_codewords(data_codewords : Slice(UInt8))
+    protected def self.draw_data_codewords(canvas : Matrix(UInt8), data_codewords : Slice(UInt8),
+                                           version : Version, ecl : ECC::Level)
+      size = canvas.size
       data_length = data_codewords.size * 8
 
       i = 0
-      upward = true      # Current filling direction
-      base_x = @size - 1 # Zig zag filling starts from bottom right
+      upward = true     # Current filling direction
+      base_x = size - 1 # Zig zag filling starts from bottom right
       while base_x > 0
-        (0...@size).reverse_each do |base_y|
+        (0...size).reverse_each do |base_y|
           (0..1).each do |alt|
             x = base_x - alt
-            y = upward ? base_y : @size - 1 - base_y
-            next if @canvas[x, y] & 0x80 > 0
+            y = upward ? base_y : size - 1 - base_y
+            next if canvas[x, y] & 0x80 > 0
             return if i >= data_length
 
             data_i = i >> 3
-            if @version == 1 && data_i == 2 ||
-               @version == 3 && @ecl.low? && data_i == 10 ||
-               @version == 3 && @ecl.medium? && data_i == 8
+            if version == 1 && data_i == 2 ||
+               version == 3 && ecl.low? && data_i == 10 ||
+               version == 3 && ecl.medium? && data_i == 8
               bit = data_codewords[data_i].bit(3 - i & 3)
               i += 1
               i += 4 if i % 4 == 0
@@ -53,7 +45,7 @@ struct Goban::MQR < Goban::AbstractQR
               i += 1
             end
 
-            @canvas[x, y] = bit
+            canvas[x, y] = bit
           end
         end
 
@@ -63,32 +55,27 @@ struct Goban::MQR < Goban::AbstractQR
     end
 
     # Test each mask patterns and apply one with the lowest (= best) score
-    protected def apply_best_mask
-      best_canvas = nil
+    protected def self.apply_best_mask(canvas : Matrix(UInt8), version : Version, ecl : ECC::Level)
+      mask, best_canvas = nil, nil
       max_score = Int32::MIN
 
       4_u8.times do |i|
-        canvas = @canvas.clone
+        c = canvas.clone
         msk = Mask.new(i)
-        CanvasDrawer.draw_format_modules(canvas, msk, @version, @ecl)
-        msk.apply_to(canvas)
+        self.draw_format_modules(c, msk, version, ecl)
+        msk.apply_to(c)
 
-        score = Mask.evaluate_score(canvas)
+        score = Mask.evaluate_score(c)
         if score > max_score
-          @mask = msk
-          best_canvas = canvas
+          mask = msk
+          best_canvas = c
           max_score = score
         end
-
-        # puts i, score
-        # canvas.print_to_console
       end
-      raise "Unable to set the mask" unless best_canvas
-      # puts @mask
+      raise "Unable to set the mask" unless mask && best_canvas
 
-      @canvas = best_canvas
-      @mask
-    end
+      {mask, best_canvas}
+   end
 
     protected def self.draw_format_modules(canvas : Matrix(UInt8), mask : Mask, ver : Version, ecl : ECC::Level)
       bits = mask.get_format_bits(ver, ecl)
--- src/goban/qr/canvasdrawer.cr
+++ src/goban/qr/canvasdrawer.cr
@@ -1,17 +1,7 @@
-require "../abstract/canvasdrawer"
-
 struct Goban::QR < Goban::AbstractQR
   # Handles painting each QR Code modules on a canvas.
-  struct CanvasDrawer < AbstractQR::CanvasDrawer
-    @mask : Mask
-    @size : Int32
-
-    # Creates a blank canvas with the given version and error correction level.
-    def initialize(@version : Version, @ecl : ECC::Level)
-      @size = @version.symbol_size
-      @canvas = Matrix(UInt8).new(@size, @size, 0)
-      @mask = Mask.new(0) # Temporal value
-    end
+  module CanvasDrawer
+    extend AbstractQR::CanvasDrawer
 
     # Draws all function patterns on the canvas.
     # The function patterns are:
@@ -19,21 +9,23 @@ struct Goban::QR < Goban::AbstractQR
     # - Finder patterns on each corner except bottom right
     # - Alignment patterns depending on the QR Code version
     # - Timing patterns in both directions
-    protected def draw_function_patterns
-      draw_pattern(0, 0, FINDER_PATTERN, 7)
-      draw_pattern(@size - 7, 0, FINDER_PATTERN, 7)
-      draw_pattern(0, @size - 7, FINDER_PATTERN, 7)
+    protected def self.draw_function_patterns(canvas : Matrix(UInt8), version : Version)
+      size = canvas.size
+
+      self.draw_pattern(canvas, 0, 0, FINDER_PATTERN, 7)
+      self.draw_pattern(canvas, size - 7, 0, FINDER_PATTERN, 7)
+      self.draw_pattern(canvas, 0, size - 7, FINDER_PATTERN, 7)
 
       # Reserving areas for the finder patterns and format info at once
       # as they belong to the same adjacent square area
-      @canvas[0, 7, 9, 2] = 0xc0
-      @canvas[7, 0, 2, 7] = 0xc0
-      @canvas[@size - 8, 0, 1, 7] = 0xc0
-      @canvas[@size - 8, 7, 8, 2] = 0xc0
-      @canvas[0, @size - 8, 8, 1] = 0xc0
-      @canvas[7, @size - 7, 2, 7] = 0xc0
+      canvas[0, 7, 9, 2] = 0xc0
+      canvas[7, 0, 2, 7] = 0xc0
+      canvas[size - 8, 0, 1, 7] = 0xc0
+      canvas[size - 8, 7, 8, 2] = 0xc0
+      canvas[0, size - 8, 8, 1] = 0xc0
+      canvas[7, size - 7, 2, 7] = 0xc0
 
-      positions = @version.alignment_pattern_positions
+      positions = version.alignment_pattern_positions
       ali_pat_count = positions.size
       ali_pat_count.times do |i|
         ali_pat_count.times do |j|
@@ -42,36 +34,37 @@ struct Goban::QR < Goban::AbstractQR
                   i == ali_pat_count - 1 && j == 0
           x, y = positions[i], positions[j]
 
-          draw_pattern(x - 2, y - 2, ALIGNMENT_PATTERN, 5)
+          self.draw_pattern(canvas, x - 2, y - 2, ALIGNMENT_PATTERN, 5)
         end
       end
 
-      draw_timing_pattern(6, @size - 16)
+      self.draw_timing_pattern(canvas, 6, size - 16)
 
-      draw_version_modules
+      self.draw_version_modules(canvas, version)
 
       canvas[8, canvas.size - 8] = 0xc1
     end
 
     # Draws data bits from the given data codewords.
-    protected def draw_data_codewords(data_codewords : Slice(UInt8))
+    protected def self.draw_data_codewords(canvas : Matrix(UInt8), data_codewords : Slice(UInt8))
+      size = canvas.size
       data_length = data_codewords.size * 8
 
       i = 0
-      upward = true      # Current filling direction
-      base_x = @size - 1 # Zig zag filling starts from bottom right
+      upward = true     # Current filling direction
+      base_x = size - 1 # Zig zag filling starts from bottom right
       while base_x > 0
         base_x = 5 if base_x == 6 # Skip vertical timing pattern
 
-        (0...@size).reverse_each do |base_y|
+        (0...size).reverse_each do |base_y|
           (0..1).each do |alt|
             x = base_x - alt
-            y = upward ? base_y : @size - 1 - base_y
-            next if @canvas[x, y] & 0x80 > 0
+            y = upward ? base_y : size - 1 - base_y
+            next if canvas[x, y] & 0x80 > 0
             return if i >= data_length
 
             bit = data_codewords[i >> 3].bit(7 - i & 7)
-            @canvas[x, y] = bit
+            canvas[x, y] = bit
             i += 1
           end
         end
@@ -82,44 +75,39 @@ struct Goban::QR < Goban::AbstractQR
     end
 
     # Test each mask patterns and apply one with the lowest (= best) score
-    protected def apply_best_mask
-      best_canvas = nil
+    protected def self.apply_best_mask(canvas : Matrix(UInt8), ecl : ECC::Level)
+      mask, best_canvas = nil, nil
       min_score = Int32::MAX
 
       8_u8.times do |i|
-        canvas = @canvas.clone
+        c = canvas.clone
         msk = Mask.new(i)
-        CanvasDrawer.draw_format_modules(canvas, msk, @ecl)
-        msk.apply_to(canvas)
+        self.draw_format_modules(c, msk, ecl)
+        msk.apply_to(c)
 
-        score = Mask.evaluate_score(canvas)
+        score = Mask.evaluate_score(c)
         if score < min_score
-          @mask = msk
-          best_canvas = canvas
+          mask = msk
+          best_canvas = c
           min_score = score
         end
-
-        # puts i, score
-        # canvas.print_to_console
       end
-      raise "Unable to set the mask" unless best_canvas
-      # puts @mask
+      raise "Unable to set the mask" unless mask && best_canvas
 
-      @canvas = best_canvas
-      @mask
+      {mask, best_canvas}
     end
 
-    private def draw_version_modules
-      return if @version < 7
+    private def self.draw_version_modules(canvas : Matrix(UInt8), version : Version)
+      return if version < 7
 
-      bits = @version.get_version_bits
+      bits = version.get_version_bits
 
       (0...18).each do |i|
         bit = (bits >> i & 1).to_u8 | 0xc0
         x = i // 3
-        y = @size - 11 + i % 3
-        @canvas[x, y] = bit
-        @canvas[y, x] = bit
+        y = canvas.size - 11 + i % 3
+        canvas[x, y] = bit
+        canvas[y, x] = bit
       end
     end
 
