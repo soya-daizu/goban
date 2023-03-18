cabd9cf38c4bb2751155b791e2a958da0ffd41ef
soya_daizu <soya_daizu@proton.me>
2023-03-19 06:54:32 +0900

    fix: update spec

--- spec/canvasdrawer_spec.cr
+++ spec/canvasdrawer_spec.cr
@@ -1,13 +1,13 @@
 require "./spec_helper"
 
 module Goban
-  struct QR::CanvasDrawer
+  module QR::CanvasDrawer
     def reserve_modules_for_test
       @canvas[8, 8, 7, 7] = 0xc0
     end
   end
 
-  struct MQR::CanvasDrawer
+  module MQR::CanvasDrawer
     def reserve_modules_for_test
       @canvas[3, 3, 3, 3] = 0xc0
     end
@@ -16,35 +16,37 @@ module Goban
   describe QR::CanvasDrawer do
     describe "#draw_function_patterns" do
       it "draws all function patterns" do
-        drawer = QR::CanvasDrawer.new(QR::Version.new(7), ECC::Level::Low)
-        drawer.draw_function_patterns
-        drawer.canvas.normalize
+        version = QR::Version.new(7)
+        canvas = Matrix(UInt8).new(version.symbol_size, version.symbol_size, 0)
+        QR::CanvasDrawer.draw_function_patterns(canvas, version)
+        canvas.normalize
 
-        rows = convert_canvas(drawer.canvas)
+        rows = convert_canvas(canvas)
         rows.should eq(FUNCTION_PATTERN_MODS_QR)
       end
     end
 
     describe "#draw_data_codewords" do
       it "fills codewords properly" do
-        drawer = QR::CanvasDrawer.new(QR::Version.new(1), ECC::Level::Low)
-        drawer.reserve_modules_for_test
+        version = QR::Version.new(1)
+        canvas = Matrix(UInt8).new(version.symbol_size, version.symbol_size, 0)
+        canvas[8, 8, 7, 7] = 0xc0
 
         codewords = Slice(UInt8).new(21 ** 2 // 8, 154)
-        drawer.draw_data_codewords(codewords)
-        drawer.canvas.normalize
+        QR::CanvasDrawer.draw_data_codewords(canvas, codewords)
 
-        rows = convert_canvas(drawer.canvas)
+        rows = convert_canvas(canvas)
         rows.should eq(CODEWORDS_FILL_MODS_QR)
       end
     end
 
     describe "#apply_best_mask" do
       it "applies best mask" do
-        drawer = QR::CanvasDrawer.new(QR::Version.new(1), ECC::Level::Low)
-        drawer.canvas.data.map_with_index! { |_, idx| idx.odd?.to_unsafe.to_u8 }
+        version = QR::Version.new(1)
+        canvas = Matrix(UInt8).new(version.symbol_size, version.symbol_size, 0)
+        canvas.data.map_with_index! { |_, idx| idx.odd?.to_unsafe.to_u8 }
 
-        drawer.apply_best_mask.value.should eq(2)
+        QR::CanvasDrawer.apply_best_mask(canvas, ECC::Level::Low)[0].value.should eq(2)
       end
     end
   end
