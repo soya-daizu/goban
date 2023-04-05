94cbca0e23de127434179782ad39f32524afe766
soya_daizu <soya_daizu@proton.me>
2023-04-06 08:13:28 +0900

    chore: update spec

--- spec/canvasdrawer_spec.cr
+++ spec/canvasdrawer_spec.cr
@@ -1,18 +1,6 @@
 require "./spec_helper"
 
 module Goban
-  module QR::CanvasDrawer
-    def reserve_modules_for_test
-      @canvas[8, 8, 7, 7] = 0xc0
-    end
-  end
-
-  module MQR::CanvasDrawer
-    def reserve_modules_for_test
-      @canvas[3, 3, 3, 3] = 0xc0
-    end
-  end
-
   describe QR::CanvasDrawer do
     describe "#draw_function_patterns" do
       it "draws all function patterns" do
@@ -54,35 +42,38 @@ module Goban
   describe MQR::CanvasDrawer do
     describe "#draw_function_patterns" do
       it "draws all function patterns" do
-        drawer = MQR::CanvasDrawer.new(MQR::Version.new(1), ECC::Level::Low)
-        drawer.draw_function_patterns
-        drawer.canvas.normalize
+        version = MQR::Version.new(1)
+        canvas = Matrix(UInt8).new(version.symbol_size, version.symbol_size, 0)
+        MQR::CanvasDrawer.draw_function_patterns(canvas)
+        canvas.normalize
 
-        rows = convert_canvas(drawer.canvas)
+        rows = convert_canvas(canvas)
         rows.should eq(FUNCTION_PATTERN_MODS_MQR)
       end
     end
 
     describe "#draw_data_codewords" do
       it "fills codewords properly" do
-        drawer = MQR::CanvasDrawer.new(MQR::Version.new(1), ECC::Level::Low)
-        drawer.reserve_modules_for_test
+        version = MQR::Version.new(1)
+        canvas = Matrix(UInt8).new(version.symbol_size, version.symbol_size, 0)
+        canvas[3, 3, 3, 3] = 0xc0
 
         codewords = Slice(UInt8).new(11 ** 2 // 8, 154)
-        drawer.draw_data_codewords(codewords)
-        drawer.canvas.normalize
+        MQR::CanvasDrawer.draw_data_codewords(canvas, codewords, version, ECC::Level::Low)
+        canvas.normalize
 
-        rows = convert_canvas(drawer.canvas)
+        rows = convert_canvas(canvas)
         rows.should eq(CODEWORDS_FILL_MODS_MQR)
       end
     end
 
     describe "#apply_best_mask" do
       it "applies best mask" do
-        drawer = MQR::CanvasDrawer.new(MQR::Version.new(1), ECC::Level::Low)
-        drawer.canvas.data.map_with_index! { |_, idx| idx.odd?.to_unsafe.to_u8 }
+        version = MQR::Version.new(1)
+        canvas = Matrix(UInt8).new(version.symbol_size, version.symbol_size, 0)
+        canvas.data.map_with_index! { |_, idx| idx.odd?.to_unsafe.to_u8 }
 
-        drawer.apply_best_mask.value.should eq(3)
+        MQR::CanvasDrawer.apply_best_mask(canvas, version, ECC::Level::Low)[0].value.should eq(3)
       end
     end
   end
